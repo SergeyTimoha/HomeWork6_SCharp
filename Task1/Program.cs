4028f859c6951f652d1856e5aceec31ea31b9974
Sergey <serega79ser@mail.ru>
2022-06-15 23:17:55 +0600

    Задача 41

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+Console.Clear();
+Console.Write("Введите количество чисел M: ");
+int size = int.Parse(Console.ReadLine());
+int[] N = new int[size].Select(x=>new Random().Next(-10,10)).ToArray();
+Console.WriteLine(String.Join(" ", N ));
+int M = N.Where(x=>x>0).Count();
+Console.Write($"Сколько чисел больше 0? -> {M} " ); 
